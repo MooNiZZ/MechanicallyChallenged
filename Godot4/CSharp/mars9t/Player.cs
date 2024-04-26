a5484ef5dc7478c5f19272a3215ceeb8f68875b8
mars_9t <mars9t.dev@gmail.com>
2024-04-26 19:59:34 +0200

    Add player movement

--- Godot4/CSharp/mars9t/Player.cs
+++ Godot4/CSharp/mars9t/Player.cs
@@ -1,15 +1,91 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
+public partial class Player : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    private static float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    private int _direction;
+    private bool _wantsToJump;
+
+    [Export]
+    public float Speed { get; set; } = 300.0f;
+
+    [Export]
+    public float JumpSpeed { get; set; } = 400.0f;
+
+    public override void _Input(InputEvent inputEvent)
+    {
+        base._Input(inputEvent);
+
+        if (inputEvent is InputEventKey keyEvent)
+        {
+            if (Input.IsActionJustPressed("jump"))
+            {
+                _wantsToJump = true;
+            }
+
+            if (Input.IsActionJustReleased("move_left"))
+            {
+                if (Input.IsActionPressed("move_right"))
+                {
+                    _direction = 1;
+                }
+                else
+                {
+                    _direction = 0;
+                }
+            }
+
+            if (Input.IsActionJustReleased("move_right"))
+            {
+                if (Input.IsActionPressed("move_left"))
+                {
+                    _direction = -1;
+                }
+                else
+                {
+                    _direction = 0;
+                }
+            }
+
+            if (Input.IsActionPressed("move_left"))
+            {
+                _direction = -1;
+            }
+            else if (Input.IsActionPressed("move_right"))
+            {
+                _direction = 1;
+            }
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        Vector2 velocity = Velocity;
+
+        if (_direction != 0)
+        {
+            velocity.X += (float)(_direction * Speed);
+            velocity.X = Mathf.Clamp(velocity.X, -Speed, Speed);
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        }
+
+        if (_wantsToJump)
+        {
+            velocity.Y -= JumpSpeed;
+            _wantsToJump = false;
+        }
+
+        velocity.Y += Gravity * (float)delta;
+
+        Velocity = velocity;
+
+        MoveAndSlide();
+    }
 }
