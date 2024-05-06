392c4e12acf4d25e79292d1ad7e07d3b8f27be3b
Derek Gooding <djgooding@live.com>
2024-05-06 08:39:35 -0400

    Update ColorIfCollidable.cs
    Redundant

--- Unity/CSharp/TuberTugger/DropDownMC/Assets/ColorIfCollidable.cs
+++ Unity/CSharp/TuberTugger/DropDownMC/Assets/ColorIfCollidable.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
+//Script is deprecated. 
 
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(BoxCollider2D))]
 public class ColorIfCollidable : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
-    private BoxCollider2D boxCollider;
+    //private SpriteRenderer spriteRenderer;
+    //private BoxCollider2D boxCollider;
 
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
+    //void Start()
+    //{
+    //    spriteRenderer = GetComponent<SpriteRenderer>();
+    //    boxCollider = GetComponent<BoxCollider2D>();
+    //}
 
-    void Update()
-    {
-        spriteRenderer.color = boxCollider.isTrigger == false ? Color.red : Color.green;
-    }
+    //void Update()
+    //{
+    //    spriteRenderer.color = boxCollider.isTrigger == false ? Color.red : Color.green;
+    //}
 }
